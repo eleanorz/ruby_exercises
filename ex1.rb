b654731e3fb1ebb996485eb59b3ef21d8b083b27
elly <eleanorz@gmail.com>
2013-10-14 17:08:45 -0700

    first push

---
+++ ex1.rb
@@ -0,0 +1,34 @@
+# Directions: In this assignment, use the things you have learned in this chapter. Create a simple program that will either add, subtract, multiply or divide two numbers and have it display the answer and the operation used.
+
+# For example, we have two variables named first_number and second_number and another variable called operation. Assign any number values to first_number and second_number and assign operation a random value ranging from 1 to 4 in which: 1 = Addition, 2 = Subtraction, 3 = Multiplication and 4 = Division. Lastly, create a variable called answer which will calculate the two numbers with respect to the operation variable.
+
+# Each time the program is run, it must display:
+
+# The answer is *answer variable*
+#   Operation used is (Either Addition, Subtraction, Multiplication or Division)
+
+
+#Functioning code below 
+
+# x = 7
+# y=11
+# z= [0..4].random
+
+# puts "The answer is", "x is equal to"
+
+# puts  z
+
+# puts" and the operation used was either addition, subtraction, Multiplication or Division"
+
+# puts "goodbye cruel world"
+
+x="John, Charles, matt, joe"
+
+puts x.split(",")
+
+y=""
+
+puts "y is empty" if y.empty?
+
+z = gets "what number?"
+puts "you said", z
